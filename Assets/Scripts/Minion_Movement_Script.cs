2b563c26cef2774045ef88f92b78f832261d63ff
owenryanul <12128015@studentmail.ul.ie>
2018-04-07 18:15:13 +0000

    Intial Commit

---
+++ Assets/Scripts/Minion_Movement_Script.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion_Movement_Script : MonoBehaviour {
+
+    private bool isMoving;
+    private float distanceRemainingToMove;
+    private Vector2 currentDirection;
+    private Vector2 nextSpaceLocation;
+    public float speed = 0.5f;
+    public float snapToDistance = 0.1f;
+
+    public Vector2 targetSpace;
+
+    //Selection Variables
+    public bool isSelected = false;
+    public BoxCollider2D selectionHitBox;
+
+
+    public Vector2 oneSpaceUpDirection = new Vector2(1.2f,2);
+    public Vector2 oneSpaceRightDirection = new Vector2(3.9f, 0);
+
+	// Use this for initialization
+	void Start () {
+        isMoving = false;
+        distanceRemainingToMove = 0.0f;
+        selectionHitBox = this.gameObject.GetComponentInChildren<BoxCollider2D>();
+        targetSpace = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        selectionLogic();
+        movementLogic();
+        
+    }
+
+    void OnMouseDown()
+    {
+        
+    }
+
+    private void selectionLogic()
+    {
+        Camera c = Camera.main;
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if(isSelected)
+            {
+                targetSpace = Camera.main.GetComponent<spaceSelection>().getSelectedSpace(this.gameObject.transform.position);
+            }
+
+            if (selectionHitBox.bounds.Contains((Vector2) c.ScreenToWorldPoint(Input.mousePosition)))
+            {
+                isSelected = true;
+            }
+            else
+            {
+                isSelected = false;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isSelected = false;
+        }
+    }
+
+    private void movementLogic()
+    {
+        if (!isMoving)
+        {
+            if (Mathf.Abs(this.gameObject.transform.position.y - targetSpace.y) > snapToDistance)
+            {
+                if (this.gameObject.transform.position.y < targetSpace.y)
+                {
+                    nextSpaceLocation = this.gameObject.transform.position + (Vector3)oneSpaceUpDirection;
+                    currentDirection = oneSpaceUpDirection;
+                    isMoving = true;
+                }
+                else if (this.gameObject.transform.position.y > targetSpace.y)
+                {
+                    nextSpaceLocation = this.gameObject.transform.position - (Vector3)oneSpaceUpDirection;
+                    currentDirection = (-oneSpaceUpDirection);
+                    isMoving = true;
+                }
+            }
+            else if (Mathf.Abs(this.gameObject.transform.position.x - targetSpace.x) > snapToDistance)
+            { 
+                if (this.gameObject.transform.position.x > targetSpace.x)
+                {
+                    nextSpaceLocation = this.gameObject.transform.position - (Vector3)oneSpaceRightDirection;
+                    currentDirection = (-oneSpaceRightDirection);
+                    isMoving = true;
+                }
+                else if (this.gameObject.transform.position.x < targetSpace.x)
+                {
+                    nextSpaceLocation = this.gameObject.transform.position + (Vector3)oneSpaceRightDirection;
+                    currentDirection = oneSpaceRightDirection;
+                    isMoving = true;
+                }
+            }
+        }
+        else
+        {
+            move(currentDirection);
+        }
+    }
+
+    private void move(Vector2 direction)
+    {
+        Vector3 direction3D = direction.normalized;
+        distanceRemainingToMove = Vector2.Distance(this.gameObject.transform.position, nextSpaceLocation);
+        if (isMoving)
+        {
+            if (distanceRemainingToMove <= snapToDistance)
+            {
+                this.gameObject.transform.position = nextSpaceLocation;
+                isMoving = false;
+            }
+            else
+            {
+                this.gameObject.transform.position += (direction3D * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private bool isWithin(float vector1, float vector2, float distance)
+    {
+        if(vector1 - vector2 <= distance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/spaceSelection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceSelection : MonoBehaviour {
+
+    public Vector2 selectedSpace;
+    public GameObject selectedThing;
+
+	// Use this for initialization
+	void Start () {
+        selectedSpace = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public Vector2 getSelectedSpace(Vector2 currentPosition)
+    {
+        Camera c = Camera.main;
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            //selectedSpace = c.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(c.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("Space"))
+            {
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+
+                selectedSpace = hit.transform.position;
+                selectedThing = hit.transform.gameObject;
+                return selectedSpace;
+            }
+        }
+        return currentPosition;
+    }
+
+}
